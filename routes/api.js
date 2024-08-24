d38f480e832e59a2a45bb39330a7ad33ed889cbf
nalyD <dylanmarin2018@gmail.com>
2024-08-24 17:52:08 -0600

    functionality works on my end, 2 get request fcc tests are not passing yet

--- routes/api.js
+++ routes/api.js
@@ -1,78 +1,158 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const crypto = require('crypto');
-const uri = process.env.MONGO_URI
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const crypto = require("crypto");
+const uri = process.env.MONGO_URI;
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 class Books {
   constructor(title) {
+    this.comments = [];
     this._id = this.generateId();
     this.title = title;
+    this.commentcount = this.comments.length;
+    this.__v = this.comments.length;
   }
   generateId() {
-    return crypto.randomBytes(12).toString('hex');
+    return crypto.randomBytes(12).toString("hex");
+  }
+  toObject() {
+    return {
+      comments: this.comments,
+      _id: this._id,
+      title: this.title,
+      commentcount: this.commentcount,
+      __v: this.__v,
+    };
   }
 }
 
-console.log(new Books)
+let collection;
 
 const run = async () => {
   try {
     await client.connect();
-    const db = client.db('database');
-    const collection = db.collection('documents');
-    console.log("You are connected!");
+    const db = client.db("library");
+    collection = db.collection("books");
+    console.log("You are connected to your DB!");
   } catch (err) {
     console.error(err);
   }
-}
+};
 
-run()
+run();
 
-
-'use strict';
+("use strict");
 
 module.exports = function (app) {
-
-  app.route('/api/books')
-    .get(function (req, res){
+  app
+    .route("/api/books")
+    .get(async function (req, res) {
+      console.log(req.params, req.url, req.method)
       //response will be array of book objects
       //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      try {
+        const result = await collection.find({}).toArray();
+        res.json(result);
+      } catch (err) {
+        if (err) console.error(err);
+      }
     })
-    
-    .post(function (req, res){
-      let title = req.body.title;
-      //response will contain new book object including atleast _id and title
 
+    .post(async function (req, res) {
+      //response will contain new book object including atleast _id and title
+      let title = req.body.title;
+      if (!title) {
+        return res.send("missing required field title");
+      } else {
+        try {
+          const newBook = new Books(title);
+          const result = await collection.insertOne(newBook.toObject());
+          console.log("Insert Result:", result);
+          res.json({
+            _id: newBook._id,
+            title: newBook.title,
+          });
+        } catch (err) {
+          if (err) console.error(err);
+        }
+      }
     })
-    
-    .delete(function(req, res){
+
+    .delete(async function (req, res) {
       //if successful response will be 'complete delete successful'
+      try {
+        const result = await collection.deleteMany({});
+        if (result) res.send("complete delete successful");
+      } catch (err) {
+        if (err) console.error(err);
+      }
     });
 
-
-
-  app.route('/api/books/:id')
-    .get(function (req, res){
+  app
+    .route("/api/books/:id")
+    .get(async function (req, res) {
+      console.log(req.params, req.url, req.method)
       let bookid = req.params.id;
+      if (!bookid) {
+        res.send("no book exists");
+      } else {
+        try {
+          const result = await collection.findOne({ _id: bookid });
+          if (!result) {
+            return res.send("no book exists");
+          } else {
+            res.json(result);
+          }
+        } catch (err) {
+          if (err) console.error(err);
+        }
+      }
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
-    
-    .post(function(req, res){
+
+    .post(async function (req, res) {
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
+      if (!comment) {
+        return res.send('missing required field comment')
+      };
+
+      try {
+        const result = await collection.updateOne(
+          { _id: bookid },
+          { $push: { comments: comment }}
+        );
+        if (result.matchedCount === 0) {
+          return res.send('no book exists')
+        }
+        const updatedBook = await collection.findOne({ _id: bookid });
+        res.json(updatedBook);
+      } catch (err) {
+        if (err) console.error(err);
+      };
+
     })
-    
-    .delete(function(req, res){
+
+    .delete(async function (req, res) {
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
+      try {
+        const result = await collection.deleteOne({ _id: bookid });
+        if (result.deletedCount === 0) {
+          return res.send('no book exists');
+        } else {
+          res.send('delete successful')
+        }
+        
+      } catch (err) {
+        if (err) console.error(err);
+      };
     });
-  
 };
